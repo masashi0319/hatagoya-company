ce85062d09e21dcbcea5ebfa88a4378b8b50e45d
masashi <no5858wish@gmail.com>
2023-06-16 22:08:05 +0900

    [update]policyページ、aboutusページ完了

---
+++ assets/js/scroll.js
@@ -0,0 +1,51 @@
+'use strict';
+
+if( window.innerWidth >= 769) {
+  let i = 0;
+  const animTargetsA = document.querySelectorAll(".js-animTargetA");
+  
+  animTargetsA.forEach(animTargetA => {
+    animTargetA.style.transitionDelay = 0 + i + 's';
+    i += 0.3;
+  });
+
+  i = 0;
+  const animTargetsB = document.querySelectorAll(".js-animTargetB");
+  
+  animTargetsB.forEach(animTargetB => {
+    animTargetB.style.transitionDelay = 0 + i + 's';
+    i += 0.3;
+  });
+}
+
+function scrollObserver(elements, timimg) {
+  const options = {
+      root: null,
+      rootMargin: `0px 0px ${timimg}px 0px`,
+      threshold: [0]
+  }
+
+  function callback(entries) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('fadeIn');
+          }
+      });
+  }
+
+  const observer = new IntersectionObserver(callback, options);
+
+  elements.forEach(element => {
+      observer.observe(element);
+  });
+}
+
+// DOM取得
+const targetsPc = document.querySelectorAll('.js_scrollPc');
+const targetsSp = document.querySelectorAll('.js_scrollSp');
+
+if( window.innerWidth >= 769) {
+  scrollObserver(targetsPc, -400); 
+} else {
+  scrollObserver(targetsSp, -200);
+}
